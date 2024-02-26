45762f1c1ba8f0ae62507c5d490b6b7874bdd310
blnchk <etkachenko@isib.ru>
2024-02-26 07:19:38 +0300

    инициализация проекта: 1. зависимости, 2. настройки БД 3. gitignore 4. класс теста

---
+++ src/main/java/org/blinchik/regionsdirectory/RegionsDirectoryApplication.java
@@ -0,0 +1,13 @@
+package org.blinchik.regionsdirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionsDirectoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegionsDirectoryApplication.class, args);
+    }
+
+}
